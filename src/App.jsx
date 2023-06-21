0b8eeffbed32ba608e19e58b61f9eeb9f56a8434
Otabek Toshtemirov <47772571+OtabekToshtemirov@users.noreply.github.com>
2023-06-21 15:03:18 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,92 @@
 import Card from "./components/Card.jsx";
 import './App.css'
 import originalData from './../data.json';
-import  {useState} from 'react';
+import {useState, useEffect} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 
 export const App = () => {
     const [data, setData] = useState(originalData);
     const [skills, setSkills] = useState([]);
 
+
     const filterList = (skill) => {
-        document.querySelector('.searchbar').style.display = 'block';
-        if (skills.includes(skill)) {
-            return;
+        if (!skills.includes(skill)) {
+            setSkills([...skills, skill]);
         }
-        setSkills([...skills, skill]);
 
-        setData(filterBySkills(originalData));
-        console.log(data)
     }
 
-    const handleRemove = (removedSkill) => {
-        setSkills(skills.filter((s) => s !== removedSkill.target.value));
+    const handleRemove = (e) => {
+        const skillToRemove = e.target.value;
+        setSkills(skills.filter((skill) => skill !== skillToRemove));
     }
 
-    const clearFilter = () =>{
+    const clearFilter = () => {
         setSkills([]);
-        document.querySelector('.searchbar').style.display = 'none';
+        setData(originalData)
     }
 
-    function filterBySkills(item) {
-        // check if the item has any of the skills in the skills array
-        return skills.some(skill => {
-            return (
-                item.role === skill ||
-                item.level === skill ||
-                item.languages.includes(skill) ||
-                item.tools.includes(skill)
-            );
-        });
+    const updateData = () => {
+        if (skills.length < 1) {
+            setData(originalData);
+        } else {
+            const newData = originalData.filter((item) => {
+                const allSkills = [...item.languages, ...item.tools, item.level, item.role];
+                return skills.every((skill) => allSkills.includes(skill));
+            });
+
+            setData(newData);
+        }
     }
 
 
-
+    useEffect(() => {
+        if (skills.length > 0) {
+            updateData();
+        } else {
+            setData(originalData);
+        }
+    }, [skills])
 
 
     return (
         <div className="wrapper">
             <div className="nav"></div>
-                <div className="container">
-                    <div className="searchbar">
-                        <div className="search">
-                            <div className={"input"}>
-                                {skills.map((skill) => {
-                                    return <span key={skill} className={"filtered-skills"}>{skill}
-                                    <button         className={"remove-button"}
+            <div className="container">
+                <div className={`${skills.length > 0 ? "searchbar" : "hidden"}`}>
+                    <div className="search">
+                        <div className={"input"}>
+                            {skills.map((skill) => {
+                                return <span key={uuidv4()} className={"filtered-skills"}>{skill}
+                                    <button
+                                        className={"remove-button"}
                                         onClick={handleRemove}
                                         value={skill}
                                     >
                                         🞫
                                     </button>
                                     </span>
-                                })}
-                            </div>
-                            <span className={'clear'}
-                            onClick={clearFilter}
-                            >Clear</span>
+                            })}
                         </div>
+                        <span className="clear"
+                              onClick={clearFilter}
+                        >Clear</span>
                     </div>
                 </div>
+            </div>
 
             <div className="container">
                 <main className="main">
                     <Card
                         data1={data}
                         filterList={filterList}
-
                     />
                 </main>
 
             </div>
 
         </div>
+
     );
 };
