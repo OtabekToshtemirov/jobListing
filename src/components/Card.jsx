caf4b93d5b5f3beaaadc6feab141a3b26fd7616f
Otabek Toshtemirov <47772571+OtabekToshtemirov@users.noreply.github.com>
2023-06-21 15:04:19 +0500

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,73 +1,73 @@
 import './card.css';
+import {v4 as uuidv4} from 'uuid';
 
 
 // eslint-disable-next-line react/prop-types
-const Card = ({data1,filterList}) => {
+const Card = ({data1, filterList}) => {
     const data = data1;
 
     const handleFilter = (skill) => {
         filterList(skill);
     }
 
-    
-
 
     return (
         <>
-            <div className="cards">
-                {data.map((data) => {
-                        return (
-                            <div className="cards__container" key={data.role}>
-                                <div className={`flex ${data.featured ? 'isFeatured' : ""}`}
-                                >
-                                    <img src={data.logo} alt=""/>
-                                    <div className="info">
-                                        <p className="company">
-                                            {data.company} {data.new ? <span className="new">New!</span> : ''}
-                                        </p>
 
-                                        {data.featured ? <span className="featured">Featured</span> : ''}<br/>
-                                        <span className={"position"}>
+            {data.map((data) => {
+                    return (
+
+                        // eslint-disable-next-line react/jsx-key
+                        <div className={`flex ${data.featured ? 'isFeatured' : ""}`} key={uuidv4()}>
+                            <img src={data.logo} alt=""/>
+                            <div className="info">
+                                <p className="company">
+                                    {data.company} {data.new ? <span className="new">New!</span> : ''}
+                                </p>
+
+                                {data.featured ? <span className="featured">Featured</span> : ''}<br/>
+                                <span className={"position"}>
                                             {data.position}
                                         </span><br/>
-                                        {data.postedAt} <span className={"dot"}>.</span>
-                                        {data.contract} <span className={"dot"}>.</span>
-                                        {data.location}
-                                    </div>
-                                    <div className="languages">
+                                {data.postedAt} <span className={"dot"}>·</span>
+                                {data.contract} <span className={"dot"}>·</span>
+                                {data.location}
+                            </div>
+                            <div className="languages">
                                         <span
                                             onClick={() => handleFilter(data.role)}
                                         > {data.role}</span>
 
-                                        <span
-                                            onClick={() => handleFilter(data.level)}
-                                        > {data.level}</span>
-                                        {data.tools.map((tool) => {
-                                            return <span
-                                                onClick={() => handleFilter(tool)}
-                                                key={data.id}
-                                            >{tool}</span>
-                                        })
-                                        }
+                                <span
+                                    onClick={() => handleFilter(data.level)}
+                                > {data.level}</span>
+                                {data.tools.map((tool) => {
+                                    return <span
+                                        onClick={() => handleFilter(tool)}
+                                        key={data.id}
+                                    >{tool}</span>
+                                })
+                                }
 
-                                        {data.languages.map((language) => {
-                                            return <span
-                                                onClick={() => handleFilter(language)}
-                                                key={language}
+                                {data.languages.map((language) => {
+                                    return <span
+                                        onClick={() => handleFilter(language)}
+                                        key={language}
 
-                                            >{language}</span>
-                                        })
-                                        }
+                                    >{language}</span>
+                                })
+                                }
 
-                                    </div>
-                                </div>
                             </div>
-                        )
+                        </div>
 
-                    }
-                )
+                    )
+
                 }
-            </div>
+            )
+            }
+
+
         </>
 
 
